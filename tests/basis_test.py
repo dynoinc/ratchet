34d858798cd7f0734ceb84c5a0b372206c6fb34c
Rajat Goel <405844+rajatgoel@users.noreply.github.com>
2024-09-20 03:42:33 -0700

    remove mypy

--- tests/basis_test.py
+++ tests/basis_test.py
@@ -1,2 +1,12 @@
-def test_add():
-    assert 1 + 2 == 3
+import pytest
+from app.main import app
+from fastapi.testclient import TestClient
+
+
+@pytest.mark.asyncio
+async def test_root() -> None:
+    client = TestClient(app)
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
