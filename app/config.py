e6e38287db95cd6632bf6975b5d875187813b724
Sandeep <sandeepkru@users.noreply.github.com>
2024-10-01 17:12:09 -0700

    [Config] Modify Configuration to seperate database fields (#3)

--- app/config.py
+++ app/config.py
@@ -1,11 +1,27 @@
 from pydantic_settings import BaseSettings
+from urllib.parse import quote_plus
 
 
 class Settings(BaseSettings):
-    database_url: str
+    database_type: str
+    database_host: str = ""
+    database_port: str = ""
+    database_name: str
+    database_user: str = ""
+    database_password: str = ""
     slack_bot_token: str
     slack_app_token: str
     openai_api_key: str
 
     class Config:
         env_file = ".env"
+
+    @property
+    def database_url(self):
+        if self.database_type == "sqlite":
+            return f"sqlite:///{self.database_name}"
+        elif self.database_type == "postgresql":
+            password = quote_plus(self.database_password)
+            return f"postgresql://{self.database_user}:{password}@{self.database_host}:{self.database_port}/{self.database_name}"
+        else:
+            raise ValueError(f"Unsupported database type: {self.database_type}")
