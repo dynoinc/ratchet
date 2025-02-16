faf0a3538cd88bf158d46fcbccc4082866b27327
Rajat Goel <405844+rajatgoel@users.noreply.github.com>
2025-02-16 15:09:04 -0800

    handle API requests with ending slash gracefully

--- internal/web/http.go
+++ internal/web/http.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/carlmjohnson/versioninfo"
@@ -90,6 +91,18 @@ func New(
 		return nil, fmt.Errorf("starting riverui server: %w", err)
 	}
 
+	withoutTrailingSlash := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
+				r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+				http.Redirect(w, r, r.URL.String(), http.StatusPermanentRedirect)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+
 	apiMux := http.NewServeMux()
 
 	// Channels
@@ -108,7 +121,7 @@ func New(
 
 	mux := http.NewServeMux()
 	mux.Handle("/riverui/", riverServer)
-	mux.Handle("/api/", http.StripPrefix("/api", apiMux))
+	mux.Handle("/api/", withoutTrailingSlash(http.StripPrefix("/api", apiMux)))
 	mux.Handle("GET /metrics", promhttp.Handler())
 	mux.Handle("GET /version", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte(versioninfo.Short()))
