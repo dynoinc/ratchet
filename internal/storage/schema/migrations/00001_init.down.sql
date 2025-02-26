1b450ca0c9f644c4eb2218d1f1c4467135e12175
Rajat Goel <405844+rajatgoel@users.noreply.github.com>
2025-02-25 23:33:22 -0800

    delete unused down migrations

--- internal/storage/schema/migrations/00001_init.down.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE messages;
-DROP TABLE channels;
-DROP TABLE incidents;
-DROP TABLE reports; 
-
--- internal/storage/schema/migrations/00002_fix_watermark_default.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE channels
-    ALTER COLUMN slack_ts_watermark
-    SET DEFAULT (EXTRACT(EPOCH FROM now() - INTERVAL '14 days')::TEXT) || '.000000';
